bb07344daefb71273f47ff00f32f5ae8d200c553
luyao-cv <1367355728@qq.com>
2024-12-19 11:19:33 +0800

    Update config.py
    paddlemix

--- HackathonBot/config.py
+++ HackathonBot/config.py
@@ -316,7 +316,7 @@ configs = [
         'type_names' : ["飞桨多模态大模型快乐开源活动"],
 
         # 每个赛题所属的赛道，每个赛道是一个数组
-        'task_types' : [['1-46']],
+        'task_types' : [['1-48']],
 
         # 该issue相关PR的前缀，用来标识PR是否属于该issue
         'pr_prefix' : "PPMix No",
